4dd7278e320028ae8a691f342ba69fca6c48b765
Gabriel Borges <gabriel@verified.inc>
2024-10-24 12:09:04 -0300

    chore: remove unused dep chore: remove unused images

--- src/components/UI/IconPlayer.tsx
+++ src/components/UI/IconPlayer.tsx
@@ -7,27 +7,11 @@ import { Box, SxProps, Theme } from '@mui/material';
 import { useEffect, useRef } from 'react';
 
 export const animations = {
-  loading: '/animations/loading.json',
-  settings: '/animations/settings.json',
-  trash: '/animations/trash.json',
-  groups: '/animations/groups.json',
-  store: '/animations/store.json',
-  home: '/animations/home.json',
-  exit: '/animations/exit.json',
-  'flat-arrow': '/animations/flat-arrow.json',
-  edit: '/animations/edit.json',
-  'add-card': '/animations/add-card.json',
-  bug: '/animations/bug.json',
-  calendar: '/animations/system-solid-23-calendar.json',
-  mail: '/animations/system-solid-191-mail.json',
-  chat: '/animations/system-solid-47-chat.json',
-  support: '/animations/system-regular-140-help-center.json',
-  copy: '/animations/copy.json',
-  avatar: '/animations/avatar.json',
-  cog: '/animations/cog.json',
   play: '/animations/system-solid-26-play-hover-play.json',
   code: '/animations/system-solid-34-code-hover-code.json',
-  credit_card: '/animations/credit_card.json',
+  settings: '/animations/settings.json',
+  store: '/animations/store.json',
+  home: '/animations/home.json',
 };
 
 export type IconPlayerProps = {
