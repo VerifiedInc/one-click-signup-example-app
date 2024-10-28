f9a5849b3dbbb3041f0df09f1dfe97a29ea58342
Gabriel Borges <gabriel@verified.inc>
2024-10-28 18:51:39 -0300

    chore: remove unused env

--- src/config.client.ts
+++ src/config.client.ts
@@ -1,15 +1,7 @@
 export interface Config {
   ENV: string;
-  demoUrl: string;
-  docsUrl: string;
-  dashboardUrl: string;
 }
 
 export const config: Config = {
   ENV: process.env.NEXT_PUBLIC_ENV || 'local',
-  demoUrl:
-    process.env.NEXT_PUBLIC_DEMO_URL || 'https://1-click.demo.verified.inc',
-  docsUrl: process.env.NEXT_PUBLIC_DOCS_URL || 'https://docs.verified.inc',
-  dashboardUrl:
-    process.env.NEXT_PUBLIC_DASHBOARD_URL || 'https://dashboard.verified.inc',
 } as const;
